23b337201d829b2ce8b3011eab3bde122945b6fa
oleksandrmurha <oleksandr.murha@gmail.com>
2019-09-04 17:22:39 +0100

    Initial Commit

---
+++ src/main/java/com/silver/bars/liveorderboard/LiveOrderBoardApplication.java
@@ -0,0 +1,13 @@
+package com.silver.bars.liveorderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveOrderBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveOrderBoardApplication.class, args);
+    }
+
+}
